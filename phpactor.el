4650f7b1d1719a639408be5235265de6ca6531df
USAMI Kenta <tadsan@zonu.me>
2018-04-15 21:52:12 +0900

    Init repository

---
+++ phpactor.el
@@ -0,0 +1,35 @@
+;;; phpactor.el --- Interface to phpactor            -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Friends of Emacs-PHP development
+
+;; Author: USAMI Kenta <tadsan@zonu.me>
+;; Created: 8 Apr 2018
+;; Version: 0.0.1
+;; Keywords: tools, php
+;; Package-Requires: ((emacs "24") (cl-lib "0.5"))
+;; URL: https://github.com/emacs-php/phpactor.el
+;; License: GPL-3.0-or-later
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; phpactor is refactoring tool for PHP.  https://github.com/phpactor/phpactor
+
+;;; Code:
+
+
+
+(provide 'phpactor)
+;;; phpactor.el ends here
