408eba7d3a0e7118695e990f50d7de7491a952f9
USAMI Kenta <tadsan@zonu.me>
2018-05-13 22:27:46 +0900

    Modify README

--- phpactor.el
+++ phpactor.el
@@ -27,6 +27,16 @@
 
 ;; Phpactor is an intelligent code-completion and refactoring tool for PHP.
 ;; https://github.com/phpactor/phpactor
+;;
+;; ## Instalation
+;;
+;; Please be aware that Phpactor is also in development stage.
+;; A simple installation method is not yet provided, but you can build it using Composer.
+;;
+;; ## Configuration
+;;
+;; See https://phpactor.github.io/phpactor/configuration.html
+;;
 
 ;;; Code:
 (require 'json)
