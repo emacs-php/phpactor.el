aa99ad4f598d67414affe53c240e84c6cc326860
USAMI Kenta <tadsan@zonu.me>
2019-02-07 23:54:52 +0900

    Fix require Emacs 24.4 and subr-x
    string-remove-prefix function is definded in subr-x feature.
    This feature is bundled with Emacs 24.4 or later.

--- phpactor.el
+++ phpactor.el
@@ -7,7 +7,7 @@
 ;; Created: 8 Apr 2018
 ;; Version: 0.1.0
 ;; Keywords: tools, php
-;; Package-Requires: ((emacs "24.3") (cl-lib "0.5") (f "0.17") (composer "0.1"))
+;; Package-Requires: ((emacs "24.4") (cl-lib "0.5") (f "0.17") (composer "0.1"))
 ;; URL: https://github.com/emacs-php/phpactor.el
 ;; License: GPL-3.0-or-later
 
@@ -48,6 +48,7 @@
 (require 'json)
 (require 'php-project)
 (require 'ring)
+(require 'subr-x)
 (require 'composer)
 
 ;; Variables
